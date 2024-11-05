7abf4b85adcb3ff8e842bb93b3f3b9cbdc4db3f2
Henry Helbock <79647569+henryHelbock@users.noreply.github.com>
2024-11-04 22:38:16 -0500

    Revert "Update marker.vue"
    This reverts commit 47136e0f82be0b2f844ac960e480dc02badf21a9.

--- src/components/marker.vue
+++ src/components/marker.vue
@@ -1,15 +1,10 @@
 <template>
-  <div
-    @click="
-      () => {
-        console.log('sdfsd')
-      }
-    "
-  >
+  <div @click="()=> {console.log('sdfsd')}">
     <slot></slot>
   </div>
 </template>
 <script>
+
 import buildComponent from './build-component.js'
 
 const props = {
@@ -117,10 +112,10 @@ export default buildComponent({
   },
 
   afterCreate(inst) {
-    events.forEach((event) => {
-      inst.addListener(event, (payload) => {
+    events.forEach((event)=> {
+      inst.addListener(event, (payload)=> {
         this.$emit(event, payload)
-      })
+      });
     })
     if (this.$clusterPromise) {
       this.$clusterPromise.then((co) => {
